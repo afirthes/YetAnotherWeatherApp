bd67c1ee4c04d5abd7a9ec77e25f131bb64ee000
afirthes <afirthes@gmail.com>
2024-01-05 12:14:23 +0300

    Initial Commit

---
+++ YetAnotherWeatherApp/YetAnotherWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YetAnotherWeatherAppApp.swift
+//  YetAnotherWeatherApp
+//
+//  Created by Icetusk on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct YetAnotherWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
